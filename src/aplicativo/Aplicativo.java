e4992f205183403295e8be34e8c2f1d4cab4c307
31618456 <31618456@087810-P33S104.macklabs.br>
2017-11-01 09:54:49 -0200

    inicializacao do projeto

---
+++ src/aplicativo/Aplicativo.java
@@ -0,0 +1,16 @@
+package aplicativo;
+
+/**
+ *
+ * @author 31618456
+ */
+public class Aplicativo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
